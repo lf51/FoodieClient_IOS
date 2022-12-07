43a268076d5363ee6956f4ad391e4ca382716d18
Lillo <lillofriscia@gmail.com>
2022-12-07 12:11:55 +0100

    Initial Commit

---
+++ FoodieClientIOS/FoodieClientIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieClientIOSApp.swift
+//  FoodieClientIOS
+//
+//  Created by Calogero Friscia on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodieClientIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
